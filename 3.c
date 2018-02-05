fe3b9387e1275528ac491f3c28107e3ee143f968
clintengeorge <35916171+clintengeorge@users.noreply.github.com>
2018-02-05 13:20:15 +0530

    Create 3.c

---
+++ 3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+void main()
+{
+    char c;
+    int isLowercaseVowel, isUppercaseVowel;
+
+    printf("Enter an alphabet: ");
+    scanf("%c"&c);
+        printf("%c is a vowel.", c);
+    else
+        printf("%c is a consonant.", c);
+    getcch();
+}
