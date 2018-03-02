3b2791dae2624b262b73217786ca595d00ac21c2
clintengeorge <35916171+clintengeorge@users.noreply.github.com>
2018-03-02 15:48:32 +0530

    Create 68.c

---
+++ 68.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int number;
+    printf("enter the number");
+    scanf("%d",&number);
+    if(number%7==0)
+    {
+        printf("yes");
+    }
+    else
+    {
+        printf("no");
+    }return 0;
+}
