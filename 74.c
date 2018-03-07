f95e4af2ea6eb1e90f9f8f7143bba42d31ec4cb6
clintengeorge <35916171+clintengeorge@users.noreply.github.com>
2018-03-07 14:02:39 +0530

    Create 74.c

---
+++ 74.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include<conio.h>
+int main(void) 
+{
+	float n;
+	int a;
+  clrscr();
+	scanf("%f",&n);
+	if(n<0)
+	{
+	    a=(int)(n-0.5);
+	}
+	else
+	{
+	    a=(int)(n+0.5);
+	}
+	printf("%d",a);
+	return 0;
+}
